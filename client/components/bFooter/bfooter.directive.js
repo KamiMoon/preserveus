8b837dc7759597d2ffe637c63ce56895b82a63a9
Eric Kizaki <erickizaki@gmail.com>
2015-11-05 21:05:56 -0600

    Scroll To Top element

--- client/components/bFooter/bfooter.directive.js
+++ client/components/bFooter/bfooter.directive.js
@@ -5,6 +5,31 @@ angular.module('preserveusApp')
         return {
             templateUrl: 'components/bFooter/footer.html',
             restrict: 'E',
-            link: function postLink(scope, element, attrs) {}
+            link: function postLink(scope, element, attrs) {
+
+                var $goToTop = $('#go-to-top');
+
+                //jQuery
+                $goToTop.click(function() {
+                    $('html, body').animate({
+                        scrollTop: 0
+                    }, 600);
+                });
+
+                //capture scroll any percentage
+                $(window).scroll(function() {
+
+                    var wintop = $(window).scrollTop(),
+                        docheight = $(document).height(),
+                        winheight = $(window).height();
+                    var scrolltrigger = 0.50;
+
+                    if ((wintop / (docheight - winheight)) > scrolltrigger) {
+                        $goToTop.show();
+                    } else {
+                        $goToTop.hide();
+                    }
+                });
+            }
         };
     });
